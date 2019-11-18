8984380fafbbfcd8db8c7c36ce549a19aaac7d18
Riv <polu3g@users.noreply.github.com>
2019-11-18 11:55:03 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello Dodenburg guys</h1>
+    <h1>Hello World {{myapidata}}</h1>
   `
 })
 export class AppComponent {
+	myapidata: any;
+constructor(private http:HttpClient) {
+	
+	this.http.get('/apihost').subscribe((_data: any) => {
+               
+            this.myapidata = _data;
+            },
+            (err: any) => {
 
+            });
+	
+    }
+	
 }
